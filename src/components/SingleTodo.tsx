c885083cb3935b49b842c712e74617f77c20427b
Jaspreet-singh-1032 <jassimehra2612@gmail.com>
2022-01-28 21:02:25 +0530

    added styling and functionality for done , edit and delete todo

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,16 +1,76 @@
-import React from 'react'
-import { Todo } from '../model'
-interface Props{
-    todo: Todo,
-    todos: Todo[],
-    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
-}
-const SingleTodo = ({todo, todos, setTodos}: Props) => {
-    return <form className='todos__single'>
-        <span className='todos__single__text'>
-           {todo.todo} 
-        </span>
-        </form>
-}
+import React, { useState, useRef, useEffect } from "react";
+import { Todo } from "../model";
+import { AiFillEdit, AiFillDelete } from "react-icons/ai";
+import { MdDone } from "react-icons/md";
+import "./SingleTodo.css";
 
-export default SingleTodo
+interface Props {
+  todo: Todo;
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+  const editInputRef = useRef<HTMLInputElement>(null);
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editedTodo, setEditedTodo] = useState<string>(todo.todo);
+  const handleDone = () => {
+    setTodos(
+      todos.map((item) =>
+        item.id === todo.id ? { ...item, isDone: !item.isDone } : item
+      )
+    );
+  };
+
+  const handleDelete = () => {
+    setTodos(todos.filter((item) => item.id !== todo.id));
+  };
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    setTodos(
+      todos.map((item) =>
+        item.id === todo.id ? { ...item, todo: editedTodo } : item
+      )
+    );
+    setEdit(false);
+  };
+
+  useEffect(() => {
+    editInputRef.current?.focus();
+  }, [edit]);
+  return (
+    <form className="todos__single" onSubmit={handleSubmit}>
+      {edit ? (
+        <input
+          type="text"
+          value={editedTodo}
+          onChange={(e) => setEditedTodo(e.target.value)}
+          className="todos__single__text"
+          ref={editInputRef}
+        />
+      ) : todo.isDone ? (
+        <s className="todos__single__text">{todo.todo}</s>
+      ) : (
+        <span className="todos__single__text">{todo.todo}</span>
+      )}
+      <div>
+        <span className="icon">
+          <AiFillEdit
+            onClick={() => {
+              if (!todo.isDone) {
+                setEdit(!edit);
+              }
+            }}
+          />
+        </span>
+        <span className="icon">
+          <AiFillDelete onClick={handleDelete} />
+        </span>
+        <span className="icon">
+          <MdDone onClick={handleDone} />
+        </span>
+      </div>
+    </form>
+  );
+};
+
+export default SingleTodo;
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,26 +1,27 @@
-import React from 'react'
-import { Todo } from '../model'
-import SingleTodo from './SingleTodo'
-import './TodoList.css'
+import React from "react";
+import { Todo } from "../model";
+import SingleTodo from "./SingleTodo";
+import "./TodoList.css";
 
-interface Props{
-    todos: Todo[],
-    setTodos: React.Dispatch<React.SetStateAction<Todo[]>>
+interface Props {
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
 }
-const TodoList = ({todos, setTodos}:Props) => {
-    return (
-        <div className='todos'>
+const TodoList = ({ todos, setTodos }: Props) => {
+  return (
+    <div className="todos">
+      {todos.map((item) => {
+        return (
+          <SingleTodo
+            key={item.id}
+            todo={item}
+            todos={todos}
+            setTodos={setTodos}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
-            {todos.map((item)=>{
-                return <SingleTodo
-                key={item.id}
-                todo={item}
-                todos={todos}
-                setTodos={setTodos}
-                />
-            })}
-        </div>
-    )
-}
-
-export default TodoList
+export default TodoList;
